872309153c252e21285a92e4b05d2e901eb19199
Dhruv0443 <dhruv0443.be21@chitkara.edu.in>
2022-06-03 00:11:10 +0530

    adding vector

--- in.cpp
+++ in.cpp
@@ -14,6 +14,26 @@ int main(){
 
     }
 
+vector<int> A{1,2,3,4,5,6};
+
+    A.pop_back();
+
+
+
+    A.push_back(7);
+
+    for (auto it=A.begin();it!=A.end();++it)
+        cout<<' '<<*it;
+
+cout<<endl;
+    if(A.empty())
+    {
+    else{cout<<"Vector isn't empty"<<endl;}
+
+    cout<<A.size()<<endl;
+
+
+
 
    return 0;
     for (i = 1; i<=5; i++){
